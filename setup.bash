c619901ba8fedd0a738a816af6a183c8ce7daaed
BeerlD <bellkunmoreanime@gmail.com>
2023-12-03 19:14:18 -0300

    fix

--- setup.bash
+++ setup.bash
@@ -1,4 +1,4 @@
-@echo off
+echo off
 
 echo Instalando chocolatey...
 START PowerShell "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))"
