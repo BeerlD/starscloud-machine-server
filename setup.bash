4a2f295851ab80a6e1a11c1d35918a3405a7350b
BeerlD <bellkunmoreanime@gmail.com>
2023-12-03 19:39:26 -0300

    fix

--- setup.bash
+++ setup.bash
@@ -3,20 +3,15 @@
 echo Instalando chocolatey...
 START /WAIT PowerShell "If(Get-Command -Name choco.exe -ErrorAction SilentlyContinue) { exit; } Else { Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')) }"
 
-echo Instalando NodeJS...
-START /WAIT PowerShell "choco upgrade nodejs.install"
-
-echo Instalando Git...
-START /WAIT PowerShell "choco upgrade git.install"
+echo Instalando Git e NodeJS...
+START /WAIT PowerShell "choco upgrade nodejs.install -y; choco upgrade git.install -y;"
 
 echo Implantando Servidor...
-cd C:\Users\
-git clone https://github.com/BeerlD/starscloud-machine-server.git
-cd starscloud-machine-server
+START /WAIT PowerShell "cd C:\Users\; git clone https://github.com/BeerlD/starscloud-machine-server.git;"
+cd C:\Users\starscloud-machine-server\
 
 echo Instalando/Configurando Pacotes...
-npm init -y
-npm install puppeteer express
+npm init -y && npm install puppeteer express
 
 echo Liberando porta de acesso...
 netsh advfirewall firewall add rule name="ApiReleaseSushinePin" dir=in action=allow protocol=TCP localport=5055
